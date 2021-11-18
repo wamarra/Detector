f5bc907431fbb37f661241c71601faeeb11a7a55
Wesley Marra <cast.wesleya.utic@sebrae.com.br>
2021-11-18 20:40:21 -0300

    Initial Commit

---
+++ Detector/DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectorApp.swift
+//  Detector
+//
+//  Created by Wesley Marra on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
